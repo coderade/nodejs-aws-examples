cb5232747afe597c8e682aef2d5a397c37f6d5dd
Valdeci Gomes <valdeci@coderade.in>
2018-05-27 18:13:02 -0300

    Update the Create Ec2 instance example code.

--- create-ec2-instance.js
+++ create-ec2-instance.js
@@ -11,8 +11,8 @@ AWS.config.update({region: 'us-east-1'});
 const ec2 = new AWS.EC2();
 
 //Security Group name
-const sgName = 'ec2_sg';
-const keyName = 'ec2_key';
+const sgName = 'ec2_examples_sg';
+const keyName = 'ec2_example_instance';
 
 createSecurityGroup(sgName)
     .then(() => {
@@ -27,8 +27,7 @@ createSecurityGroup(sgName)
     })
     .catch((err) => {
         console.error('Failed to create instance with:', err)
-    })
-;
+    });
 
 // Create function
 
@@ -71,9 +70,8 @@ function createSecurityGroup(sgName) {
                 ec2.authorizeSecurityGroupIngress(params, (err) => {
                     if (err)
                         reject(err);
-                    else {
-
-                    }
+                    else
+                        resolve();
                 });
             }
         })
@@ -98,11 +96,13 @@ function createInstance(sgName, keyName) {
         ImageId: 'ami-14c5486b', //AMI ID that will be used to create the instance
         InstanceType: 't2.micro',
         KeyName: keyName,
+        Name: keyName,
         MaxCount: 1,
         MinCount: 1,
         SecurityGroups: [
             sgName
         ],
+        //The UserData base64 encoded from ec2-startup.sh file.
         UserData: 'IyEvYmluL2Jhc2gNCmN1cmwgLS1zaWxlbnQgLS1sb2NhdGlvbiBodHRwczovL3JwbS5ub2Rlc291cmNlLmNvbS9zZXR1cF8xMC54IHwgc3VkbyBiYXNoIC0NCnN1ZG8geXVtIGluc3RhbGwgLXkgbm9kZWpzDQpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9jb2RlcmFkZS9hd3MtZWMyLWV4YW1wbGVzDQpjZCBoYmZsDQpucG0gaQ0KbnBtIHJ1biBzdGFydA=='
     };
 
